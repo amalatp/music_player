3acd0a3a2e85919a9d7158d3b09d0e58eca43edc
amala <amalahameedtp@gmail.com>
2026-01-19 15:48:45 +0530

    Integrate Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:music_player/core/configs/theme/app_theme.dart';
+import 'package:music_player/firebase_options.dart';
 import 'package:music_player/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:music_player/presentation/splash/pages/splash_screen.dart';
 import 'package:path_provider/path_provider.dart';
@@ -14,6 +16,7 @@ Future<void> main() async {
         ? HydratedStorageDirectory.web
         : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
